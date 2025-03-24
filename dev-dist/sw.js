92b34373eb864c7e1160c956b1e63547e2a9b725
sarim-alias <sarimsitenative@gmail.com>
2025-03-24 13:49:02 +0500

    feat/Sports clone

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.vreo1t1vfeo"
+    "revision": "0.ssnfms0vui"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
